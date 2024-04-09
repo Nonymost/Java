9df918307236f938247c26f86a40ee768e492adf
Nonymost <maharjananujraja@gmail.com>
2024-04-09 17:27:34 +0545

    java

---
+++ Operators.java
@@ -0,0 +1,22 @@
+
+public class Operators {
+    public static void main(String[] args) {
+        // Arithmetic Operators -> + , - , % , / , *, ++, --
+        double num1, num2, exp;
+        num1 = 20;
+        num2 = 23;
+
+        exp = num1 + num2;
+        exp = num1 - num2;
+        exp = 20 % 23;
+        exp = 20 / 2;
+        exp = num1++;
+        exp = ++num1;
+        exp = num1--;
+        exp = --num1;
+
+        // Assignment Operators -> =, +=,-=,*=,/=,%=
+        // Comparision Operators -> ==, <=, >=, !=, <, >
+        // Logical Operators -> &&, ||, !
+    }
+}
--- TypeCasting.java
+++ TypeCasting.java
@@ -1,16 +1,18 @@
+import java.io.*;
+
 public class TypeCasting {
     public static void main(String[]args){
-        // Wide Casting(Automatic)
-        int a = 100;
-        double b = a;
+        // Wide Casting(Automatic/Implicit)
+        int aInt = 100;
+        double bDouble = aInt;
 
-        //Narrow Casting(Manual)
-        double aD = 100.244;
-        int bD = (int)aD;
+        //Narrow Casting(Manual/Explicit)
+        double aDouble = 100.244;
+        int bInt = (int)aDouble;
         
-        System.out.println(a);
-        System.out.println(b);
-        System.out.println(aD);
-        System.out.println(bD);
+        System.out.println(aInt);
+        System.out.println(bDouble);
+        System.out.println(aDouble);
+        System.out.println(bInt);
     }
 }
