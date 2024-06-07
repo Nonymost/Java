dc45227d50dd97ad9f6f93db68227a29bcca658f
Nonymost <maharjananujraja@gmail.com>
2024-06-07 14:50:25 +0545

    commit

---
+++ Networking/JNet.java
@@ -0,0 +1,11 @@
+import java.net.*;
+
+public class JNet {
+    public static void main(String[] args) {
+        try {
+            public ServerSocket(int 443) throws
+        } catch (Exception e) {
+            System.out.println("Error " + e);
+        }
+    }
+}
